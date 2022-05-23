92d49d4bf43afe36ce070cf6078aab01e745f5a7
Joel Simpson <270112350@yoobeestudent.ac.nz>
2022-05-24 10:02:16 +1200

    test

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('home.html')
+
+@app.route('/welcome')
+def welcome():  # put application's code here
+    return render_template('welcome.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
